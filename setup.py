9d4e7af759d8d3cc1b36897b279ebb3d53755e2f
cmyui <cmyuiosu@gmail.com>
2022-10-24 01:37:32 -0400

    fix: install subdepencies at installation time

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup
 
-setup()
+requirements = []
+with open("requirements.txt", "r") as f:
+    for raw_req in f.read().splitlines():
+        # TODO: dear god this is not it
+        if raw_req.startswith("git+"):
+            req_name = raw_req.rsplit("/", 1)[1]
+            requirements.append(f"{req_name} @ {raw_req}")
+        else:
+            requirements.append(raw_req)
+
+setup(
+    install_requires=requirements,
+)
